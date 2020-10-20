44e0dd547c2e3705c44a25d1caeee80a56249dc8
Josehower <josehowerupn@gmail.com>
2020-10-20 15:14:52 +0200

    Create all the assets for the Heroku deployment

--- utils/dataBase.js
+++ utils/dataBase.js
@@ -1,8 +1,17 @@
 import postgres from 'postgres';
 import dotenv from 'dotenv';
+import { setPostgresDefaultsOnHeroku } from '../setPostgresDefaultsOnHeroku';
+
+setPostgresDefaultsOnHeroku();
 
 dotenv.config();
-const sql = postgres();
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 export async function getInventory() {
   const products = await sql`
